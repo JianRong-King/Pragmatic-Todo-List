e9724539756d6cea674ef93b1ec87c3b36170510
King Jian Rong <jkin0029@student.monash.edu>
2024-11-14 23:44:54 +0800

    Added place holder and functions towards formation of adding task

--- project_todo/src/components/TodoWrapper.js
+++ project_todo/src/components/TodoWrapper.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TodoForm } from './TodoForm';
+import {v4 as uuidv4} from 'uuid';
+import { Todo } from './todo';
+uuidv4();
 
 export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}
+        ]);
+        console.log(todos);
+    }
     return (
         <div className='TodoWrapper'>
-            <TodoForm />
+            <TodoForm  addTodo = {addTodo}/>
+            <Todo />
+
         </div>
     )
 };
+
+// Our app.js now has a single component TodoWrapper
+// in its div
+// rememeber div is a container for elements
