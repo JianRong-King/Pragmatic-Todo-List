b84b06f7f34d8403c919c881ccd5a5052072569c
King Jian Rong <jkin0029@student.monash.edu>
2024-11-15 17:02:26 +0800

    Enable toggle complete by adding a completed and un/ css

--- project_todo/src/components/TodoWrapper.js
+++ project_todo/src/components/TodoWrapper.js
@@ -1,39 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { TodoForm } from './TodoForm';
-import {v4 as uuidv4} from 'uuid';
-import { Todo } from './Todo';
-uuidv4();
+import { v4 as uuidv4 } from 'uuid';
+import { Todo } from './todo';
 
 export const TodoWrapper = () => {
     const [todos, setTodos] = useState([]);
 
-
-    const addTodo = todo => {
-        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}
+    const addTodo = (todo) => {
+        setTodos([
+            ...todos,
+            { id: uuidv4(), task: todo, completed: false, isEditing: false },
         ]);
-        // console.log(todos);
-    }
+    };
 
-    const toggleComplete = id => {
-        setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo));
-    }
+    const toggleComplete = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
 
-
-    return ( // what we want to show on the browser
-        <div className='TodoWrapper'>
+    return (
+        <div className="TodoWrapper">
             <h1>Todo Of The Day!</h1>
-            <TodoForm  addTodo = {addTodo}/>
-            {
-                todos.map((todo, index) => (
-                    <Todo task ={todo} key={index} 
-                    toggleComplete={toggleComplete}
-                    />))
-            }
-
+            <TodoForm addTodo={addTodo} />
+            {todos.map((todo) => (
+                <Todo key={todo.id} task={todo} toggleComplete={toggleComplete} />
+            ))}
         </div>
-    )
+    );
 };
-
-// Our app.js now has a single component TodoWrapper
-// in its div
-// rememeber div is a container for elements
--- project_todo/src/components/todo.js
+++ project_todo/src/components/todo.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faTrash} from '@fortawesome/free-solid-svg-icons';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = ({task, toggleComplete}) => {
+export const Todo = ({ task, toggleComplete }) => {
     return (
-        <div className = 'Todo'>
-            <p onClick = {()=> toggleComplete(task.id)} 
-            className = {`${task.completed ? 'completed' : ""}`}>{task.task}</p>
+        <div className="Todo">
+            <p
+                className={`${task.completed ? "completed" : "incompleted"}`}
+                onClick={() => toggleComplete(task.id)}
+            >
+                {task.task}
+            </p>
             <div>
-                <FontAwesomeIcon icon = {faPenToSquare} />
-                <FontAwesomeIcon icon = {faTrash} />
+                <FontAwesomeIcon icon={faPenToSquare} />
+                <FontAwesomeIcon icon={faTrash} />
             </div>
         </div>
-    )
-}
+    );
+};
