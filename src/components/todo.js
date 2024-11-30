6875c2af82b7cb39e48e2514c21f0c2a106027fe
King Jian Rong <jkin0029@student.monash.edu>
2024-11-30 20:24:51 +0800

    Removed unused import

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import { EditTodoForm } from './EditToDoForm';
+
 
 export const Todo = ({ task, toggleComplete, deleteTodo , editTodo}) => {
     return (
